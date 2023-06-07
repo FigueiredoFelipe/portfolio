7581aca708f350bfbfeb73d45e99caa8249d4be0
Felipe <felipenegreirosadv@gmail.com>
2023-06-06 21:29:59 -0300

    fix: header

--- src/components/MainHeader/Navbar.tsx
+++ src/components/MainHeader/Navbar.tsx
@@ -1,21 +1,12 @@
-// import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 export function Navbar() {
-  // const navRef = useRef();
-
-  // const showNavbar = () => {
-  //   navRef.current.classList.toggle("responsive_nav");
-  // };
-
   return (
     <header className="box flex justify-between items-center p-3 font-Poppins">
       <h3 className="text-gray-800 text-xl cursor-pointer block font-bold my-3 px-10">
         Felipe.dev
       </h3>
-      <nav
-        className="flex gap-6 font-semibold text-base p-4 pr-16" /*ref={navRef}*/
-      >
+      <nav className="flex gap-6 font-semibold text-base p-4 pr-16">
         <a href="/#" className="hover:text-blue-500">
           Home
         </a>
