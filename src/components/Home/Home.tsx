561c824b831727841d96ea2d45f45de91bdc1523
Felipe <felipenegreirosadv@gmail.com>
2023-06-21 18:22:46 -0300

    fix: techStack-component
    fixed the style and responsivety

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -8,16 +8,16 @@ export const Home = forwardRef<HTMLElement>((_, ref) => {
   return (
     <section className="font-Poppins text-gray-800" ref={ref}>
       <div className="bg-gray-100 shadow-inner py-12 p-12 justify-center">
-        <div className="flex justify-center">
-          <div className="max-w-[38rem]">
-            <h1 className="text-[3.4rem] font-bold pt-12 max-w-[45rem] mb-[1rem] mt-[2rem] leading-[1.2]">
+        <div className="flex justify-center border-solid border-blue-500 border">
+          <div className="max-w-[38rem] border-solid border-solid border-red-500 border">
+            <h1 className="text-[3.4rem] font-bold pt-12 max-w-[45rem] mb-[1rem] mt-[2rem] leading-[1.2] border-solid border-red-500 border">
               Front-End React Developer
             </h1>
-            <p className="max-w-[30rem] text-[1rem] pb-5">
+            <p className="max-w-[30rem] text-[1rem] pb-5 border-solid border-red-500 border">
               Hi, I'm Felipe Figueiredo. A passionate Front-end React Developer
               based in Belo Horizonte, Brazil.
             </p>
-            <div className="text-3xl flex space-x-2">
+            <div className="text-3xl flex space-x-2 border-solid border-red-500 border">
               <a
                 href="https://www.linkedin.com/in/fjnfigueiredo/"
                 target="_blank"
@@ -34,7 +34,7 @@ export const Home = forwardRef<HTMLElement>((_, ref) => {
               </a>
             </div>
           </div>
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center border-solid border-red-500 border">
             <img
               className="rounded-full"
               src="https://media.licdn.com/dms/image/D4D03AQEIVJnHZV2jJQ/profile-displayphoto-shrink_200_200/0/1683295089907?e=1691625600&v=beta&t=C3H4Kle9mZ7WKyX1gOFZ7iD9zOKf1h0NUW7gBPLm8YM"
@@ -42,7 +42,7 @@ export const Home = forwardRef<HTMLElement>((_, ref) => {
             />
           </div>
         </div>
-        <div className="">
+        <div className="border-solid">
           <TechStack />
         </div>
       </div>
--- src/components/Home/TechStack.tsx
+++ src/components/Home/TechStack.tsx
@@ -1,17 +1,18 @@
-import { FaHtml5, FaCss3Alt, FaBootstrap, FaReact } from "react-icons/fa";
+import { FaHtml5, FaCss3Alt, FaReact } from "react-icons/fa";
 
 import { SiTailwindcss, SiJavascript } from "react-icons/si";
 
 export function TechStack() {
   return (
-    <div className="flex bg-gray-100 justify-center text-[1.5rem] leading-6 space-x-3 p-14">
-      <p>Tech Stack |</p>
-      <FaHtml5 />
-      <FaCss3Alt />
-      <SiJavascript />
-      <SiTailwindcss />
-      <FaBootstrap />
-      <FaReact />
+    <div className="flex flex-col md:flex-row bg-gray-100 justify-center text-2xl leading-6 space-x-1 py-14">
+      <span className="flex justify-center">Tech Stack |</span>
+      <div className="flex flex-wrap justify-center items-center space-x-1 py-2 md:py-0">
+        <FaHtml5 class="fill-red-500" />
+        <FaCss3Alt class="fill-blue-500" />
+        <SiJavascript class="fill-yellow-400" />
+        <SiTailwindcss class="fill-blue-400" />
+        <FaReact class="fill-blue-500" />
+      </div>
     </div>
   );
 }
